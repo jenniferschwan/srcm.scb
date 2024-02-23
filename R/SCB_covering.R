8394743363894b455255f4a97125ee409091f038
Jennifer Schwan <schwan@fh-aachen.de>
2024-02-23 12:00:36 +0100

    added scb_subset

--- R/SCB_covering.R
+++ R/SCB_covering.R
@@ -1,11 +1,11 @@
 #' Checks whether the SCB fully covers the true survival function or not
 #'
 #' @param SCB the given SCB, must contain the columns 'time', 'SCB_low' and 'SCB_high'
-#' @param true_S the true survival function
+#' @param S_true the true survival function
 #'
 #' @return TRUE if the SCB fully covers the true survival function, FALSE if not
-SCB_covering <- function(SCB, true_S) {
+SCB_covering <- function(SCB, S_true) {
   checkmate::assert_names(names(SCB), must.include = c("time", "SCB_low", "SCB_high"))
 
-  all(SCB$SCB_high >= true_S(SCB$time)) && all(true_S(SCB$time)[-1] >= SCB$SCB_low[-nrow(SCB)])
+  all(SCB$SCB_high >= S_true(SCB$time)) && all(S_true(SCB$time)[-1] >= SCB$SCB_low[-nrow(SCB)])
 }
---
+++ R/SCB_get_quantile_subset.R
@@ -0,0 +1,21 @@
+#' Slices the SCB according to the given quantiles
+#'
+#' @param SCB the SCB which should be made a subset of itself, must include column 'time'
+#' @param lower_quantile the lower quantile (between 0 and 1)
+#' @param upper_quantile the upper quantile (between 0 and 1)
+#'
+#' @return A subset of the input SCB
+SCB_get_quantile_subset <- function(SCB, lower_quantile = 0, upper_quantile = 1) {
+  checkmate::assert_names(names(SCB), must.include = "time")
+  checkmate::assert_double(lower_quantile, lower = 0, upper = 1)
+  checkmate::assert_double(upper_quantile, lower = 0, upper = 1)
+
+  idx <- which(
+    SCB$time >= SCB$time[floor(nrow(SCB) * lower_quantile) + 1]
+    &
+    SCB$time <= SCB$time[floor(nrow(SCB) * upper_quantile)]
+    )
+
+  dplyr::slice(SCB, idx)
+
+}
