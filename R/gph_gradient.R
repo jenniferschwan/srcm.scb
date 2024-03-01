de26d2c73467f7ba6989ba2b4ddca003c3a2532f
Jennifer Schwan <schwan@fh-aachen.de>
2024-03-01 19:40:36 +0100

    added export for gph_gradient

--- R/gph_gradient.R
+++ R/gph_gradient.R
@@ -1,3 +1,12 @@
+#' Title
+#'
+#' @param t
+#' @param params
+#'
+#' @return
+#' @export
+#'
+#' @examples
 gph_gradient <- function(t, params) {
   t^params[2] / (params[1] + t^params[2]) * c(1, params[1] * log(t))
 }
